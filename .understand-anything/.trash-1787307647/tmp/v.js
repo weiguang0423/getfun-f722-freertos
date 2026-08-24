a504227765e8c80120e29ac75c7e5148d47239ba
weiguang0423 <1364609857@qq.com>
2026-08-24 11:21:00 +0800

    delete

--- .understand-anything/.trash-1787307647/tmp/v.js
+++
@@ -1,9 +0,0 @@
-const r = require('E:/getfun-f722-freertos/GETFUN_F722_FreeRTOS/.understand-anything/tmp/ua-tour-results.json');
-const idx = r.nodeSummaryIndex;
-// print all node ids containing 'Tools' and 'ld' and 'startup' and 'RK3568/s7_6' and 's7_5'
-const want = ['Tools/', 'STM32F722XX_FLASH', 'startup_stm32', 'linux_rc', 'rc_source', 'rc_task', 'platform_time', 'platform_diag'];
-for (const id of Object.keys(idx)) {
-  for (const w of want) {
-    if (id.includes(w)) { console.log(id, '::', idx[id].name); break; }
-  }
-}
